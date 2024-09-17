42f7696c9a028533781f27ab917408b7032bff93
BT_2S14_26 <arshtrashmails@gmail.com>
2024-09-17 10:55:52 -0400

    Initialized Development Env & declared nessecary variables

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("\033[0;1m" + "Welcome to Tip Calculator");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter your Subtotal: ");
+        String bill = scanner.nextLine();
+
+        String input = scanner.nextLine();
+        String[] numTipValues = input.split(" ");
+        System.out.println("What is your tip percentage (or range of values): ");
+
+        int len = numTipValues.length;
+        double[] numbers = new double[len];
+
+        for (int i = 0; i < len; i++) {
+            numbers[i] = Double.parseDouble(numTipValues[i]);
+        }
+
+        numbers.stream().mapToInt(Integer::intValue).sum();
+        numbers.stream().reduce(1, (a,b) -> a*b)};
     }
 }

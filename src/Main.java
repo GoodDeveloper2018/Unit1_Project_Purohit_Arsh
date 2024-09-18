da923b5b4dc135b2bf2b95ca967da12d5dc176ce
BT_2S14_26 <arshtrashmails@gmail.com>
2024-09-18 10:17:43 -0400

    Made string List to take in range of tip values to calculate multiple Bills from subtotal to give user Options.

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,20 @@ import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) {
+        //Introduction to Tip calculator
         System.out.println("\033[0;1m" + "Welcome to Tip Calculator");
-        Scanner scanner = new Scanner(System.in);
+        //Creates new Scanner to take input of subTotal amount
+        Scanner subTotal = new Scanner(System.in);
+        //Sout statement for user to input SubTotal
         System.out.println("Please Enter your Subtotal: ");
-        String bill = scanner.nextLine();
 
+        //Create new scanner to take in tip inputs
+        Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        String[] numTipValues = input.split(" ");
         System.out.println("What is your tip percentage (or range of values): ");
+        String[] numTipValues = input.split(" ");
+        scanner.nextLine();
+        System.out.println(numTipValues[0]);
 
         int len = numTipValues.length;
         double[] numbers = new double[len];
@@ -20,7 +26,5 @@ public class Main {
             numbers[i] = Double.parseDouble(numTipValues[i]);
         }
 
-        numbers.stream().mapToInt(Integer::intValue).sum();
-        numbers.stream().reduce(1, (a,b) -> a*b)};
     }
 }
